fe5813f1236273ddff5e08c50bada40b657bbf69
Floki <ypr_tylerkeuler@aol.com>
2022-06-24 10:28:48 -0400

    handled the logic to take information from local storage and fs write to the page

---
+++ helpers/fs-utils.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const util = require('util');
+const { param } = require('../routes');
+
+const readFromFIle = util.promisify(fs.readFile);
+
+@param{string}
+@param{object}
+@returns{void}
+
+const writeToFile = (destination, content) => {
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => {
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    });
+}
+
+@param{string}
+@param{object}
+@returns{void}
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data)=> {
+        if (err) {
+            console.error(err);
+        } else{
+            const parsedData = JSON.parse(data);
+        }
+    });
+};
+
+module.exports = {readFromFIle, writeToFile, readAndAppend}
